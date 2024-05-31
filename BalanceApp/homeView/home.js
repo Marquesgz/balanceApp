5a09fcb77aa7303c60f08d1c2626bc595be3eb24
Johnny G <160162271+Marquesgz@users.noreply.github.com>
2024-05-31 12:16:49 +0100

    Delete BalanceApp directory

--- BalanceApp/homeView/home.js
+++
@@ -1,6 +0,0 @@
-let anims = [...document.querySelectorAll("[anim]")];
-console.log(anims);
-let click = (el, cb) => el.addEventListener("click", cb);
-let toggle = (el) => el.classList.toggle("toggled");
-let clickTog = (el) => click(el, () => toggle(el));
-anims.map(clickTog);
